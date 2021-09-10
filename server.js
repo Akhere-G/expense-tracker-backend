d95b7f0fb52ecbdc1477277a7b730402ed159b32
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-11 00:08:15 +0100

    connects to mongoose database

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
 const { graphqlHTTP } = require("express-graphql");
-
 const schema = require("./schema/schema");
-const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+const cors = require('cors')
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+const CONNECTION_URL = process.env.CONNECTION_URL
 
 app.use(
   "/graphql",
@@ -13,5 +23,11 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Listening for requests at port ${PORT}`));
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch(err => console.log(`Could not connect. ${err.message}`))
