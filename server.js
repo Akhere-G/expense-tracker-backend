a3365396d88b80804df7a2abdf1c117306d7fa59
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-09-22 17:54:44 +0100

    removed unsued import

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const path = require("path")
 
 const transactionRoutes = require("./routes/transactions");
 const authRoutes = require("./routes/auth");
